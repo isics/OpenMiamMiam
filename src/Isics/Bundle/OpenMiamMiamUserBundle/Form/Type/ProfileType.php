9c8d4789a477c33c2dae3b77731fb3e6b2088038
Sébastien Cas <scas@MacBook-Air-de-Sebastien.local>
2013-09-18 22:49:11 +0200

    Added registration and profile editing process

---
+++ src/Isics/Bundle/OpenMiamMiamUserBundle/Form/Type/ProfileType.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the OpenMiamMiam project.
+ *
+ * (c) Isics <contact@isics.fr>
+ *
+ * This source file is subject to the AGPL v3 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Isics\Bundle\OpenMiamMiamUserBundle\Form\Type;
+
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileType extends BaseType
+{
+    /**
+     * @see AbstractType
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->add('firstname', 'text', array('required' => false))
+                ->add('lastname', 'text')
+                ->add('address1', 'text')
+                ->add('address2', 'text', array('required' => false))
+                ->add('zipcode', 'text')
+                ->add('city', 'text')
+                ->remove('current_password');
+    }
+
+    /**
+     * @see AbstractType
+     */
+    public function getName()
+    {
+        return 'open_miam_miam_user_profile';
+    }
+}
