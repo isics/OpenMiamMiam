9f349c440b340f6a6675f0783ddc6098afac028a
Nicolas Charlot <nicolas.charlot@isics.fr>
2013-09-20 00:52:28 +0200

    Updated metrics script to add YAML stats

--- stats.sh
+++ stats.sh
@@ -12,7 +12,11 @@ less_lines=`cat $less_files | wc -l`
 js_files=`find src/ -name "*.js"`
 js_lines=`cat $js_files | wc -l`
 
+yaml_files=`find src/ -name "*.yml"`
+yaml_lines=`cat $yaml_files | wc -l`
+
 echo "$php_lines lines of PHP"
 echo "$twig_lines lines of Twig"
 echo "$less_lines lines of Less"
 echo "$js_lines lines of Javascript"
+echo "$yaml_lines lines of YAML"
