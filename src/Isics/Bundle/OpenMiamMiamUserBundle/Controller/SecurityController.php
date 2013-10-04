66ff7c43adb39b2a20e4c4afb41403a2f8daa3a8
Sébastien Cas <scas@MacBook-Air-de-Sebastien.local>
2013-10-04 19:21:18 +0200

    Added registration confirmation process

--- src/Isics/Bundle/OpenMiamMiamUserBundle/Controller/SecurityController.php
+++ src/Isics/Bundle/OpenMiamMiamUserBundle/Controller/SecurityController.php
@@ -10,9 +10,56 @@
 namespace Isics\Bundle\OpenMiamMiamUserBundle\Controller;
 
 use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Exception\DisabledException;
 
 class SecurityController extends BaseController
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function loginAction(Request $request)
+    {
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            if ($error instanceof DisabledException) {
+
+                $user = $this->container->get('fos_user.user_manager')->findUserByEmail($error->getToken()->getUser());
+                if (null !== $user) {
+                    $this->container->get('fos_user.mailer')->sendConfirmationEmailMessage($user);
+                }
+            }
+
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
+
+        $csrfToken = $this->container->has('form.csrf_provider')
+                ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
+                : null;
+
+        return $this->renderLogin(array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'csrf_token' => $csrfToken,
+        ));
+    }
+
     /**
      * {@inheritDoc}
      */
