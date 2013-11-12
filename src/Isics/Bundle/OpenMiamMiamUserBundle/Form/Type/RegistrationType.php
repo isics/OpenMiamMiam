b48da7c683990246d0f662e120283dea5287dd45
Dimitri <dimitri.anciaux@gmail.com>
2013-11-12 15:42:47 +0100

    Added user's phone
    Conflicts:
    	src/Isics/Bundle/OpenMiamMiamUserBundle/Resources/views/Profile/edit.html.twig

--- src/Isics/Bundle/OpenMiamMiamUserBundle/Form/Type/RegistrationType.php
+++ src/Isics/Bundle/OpenMiamMiamUserBundle/Form/Type/RegistrationType.php
@@ -29,6 +29,7 @@ class RegistrationType extends BaseType
                 ->add('address2', 'text', array('required' => false))
                 ->add('zipcode', 'text')
                 ->add('city', 'text')
+                ->add('phone', 'text', array('required' => false))
                 ->remove('username');
     }
 
