a0b91befb468a1b1d8e1bea6ec6b2b7432397ab4
Dimitri <dimitri.anciaux@gmail.com>
2013-12-02 14:30:10 +0100

    Added sort by reference

--- src/Isics/Bundle/OpenMiamMiamBundle/Entity/Repository/SalesOrderRepository.php
+++ src/Isics/Bundle/OpenMiamMiamBundle/Entity/Repository/SalesOrderRepository.php
@@ -90,6 +90,7 @@ class SalesOrderRepository extends EntityRepository
                 ->andWhere('so.branchOccurrence = :branchOccurrence')
                 ->setParameter('branchOccurrence', $branchOccurrence)
                 ->addOrderBy('so.id')
+                ->addOrderBy('sor.ref')
                 ->getQuery()
                 ->getResult();
     }
