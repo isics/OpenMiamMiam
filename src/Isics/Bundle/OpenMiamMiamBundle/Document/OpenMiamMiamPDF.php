f9ca6683a6acd25f12f0a58055cad6f24eca0b6c
Julien Pottier <julien.pottier@isics.fr>
2014-06-20 10:46:49 +0200

    Correction on PDF page number style

--- src/Isics/Bundle/OpenMiamMiamBundle/Document/OpenMiamMiamPDF.php
+++ src/Isics/Bundle/OpenMiamMiamBundle/Document/OpenMiamMiamPDF.php
@@ -9,8 +9,8 @@ class OpenMiamMiamPDF extends TCPDF
     public function Footer() {
         $this->SetY(-15);
         // Set font
-        $this->SetFont('helvetica', 'I', 8);
+        $this->SetFont('helvetica', '', 9);
         // Page number
-        $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'R', 0, '', 0, false, 'T', 'M');
+        $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().' / '.$this->getAliasNbPages(), 0, false, 'R', 0, '', 0, false, 'T', 'M');
     }
 } 
