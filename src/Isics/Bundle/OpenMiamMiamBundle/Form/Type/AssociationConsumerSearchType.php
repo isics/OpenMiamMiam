cf10c641172325b9fe31161e6e516677cde799d1
Matthieu <matthieulescieux@hotmail.fr>
2014-06-23 00:14:18 +0200

    Form type

---
+++ src/Isics/Bundle/OpenMiamMiamBundle/Form/Type/AssociationConsumerSearchType.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 22/06/14
+ * Time: 23:57
+ */
+
+namespace Isics\Bundle\OpenMiamMiamBundle\Form\Type;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class AssociationConsumerSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'ref',
+                'text',
+                array(
+                    'required' => false
+                )
+            )
+            ->add(
+                'lastName',
+                'text',
+                array(
+                    'required' => false
+                )
+            )
+            ->add(
+                'firstName',
+                'text',
+                array(
+                    'required' => false
+                )
+            )
+            ->add(
+                'creditor',
+                'choice'
+            )
+            ->add('filter', 'submit');
+    }
+
+    /**
+     * @see AbstractType
+     */
+    public function getName()
+    {
+        return 'open_miam_miam_association_consumer_search';
+    }
+} 
