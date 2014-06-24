187dfca7d449ad73cc9ddadda551050ff1040a80
Matthieu <matthieulescieux@hotmail.fr>
2014-06-24 13:37:55 +0200

    Filters working right

--- src/Isics/Bundle/OpenMiamMiamBundle/Form/Type/AssociationConsumerSalesOrderSearchType.php
+++ src/Isics/Bundle/OpenMiamMiamBundle/Form/Type/AssociationConsumerSalesOrderSearchType.php
@@ -35,6 +35,7 @@ class AssociationConsumerSalesOrderSearchType extends AbstractType
         $association = $options['association'];
 
         $builder
+            ->setMethod('GET')
             ->add(
                 'branch',
                 'entity',
