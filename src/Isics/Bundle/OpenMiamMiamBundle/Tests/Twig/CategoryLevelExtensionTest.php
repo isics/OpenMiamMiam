c7d964cef262fa7fcf732c5c2594d8325cb2d015
Steve Robinet <steve.robinet@isics.fr>
2014-08-26 13:48:52 +0200

    Fix Typo

--- src/Isics/Bundle/OpenMiamMiamBundle/Tests/Twig/CategoryLevelExtensionTest.php
+++ src/Isics/Bundle/OpenMiamMiamBundle/Tests/Twig/CategoryLevelExtensionTest.php
@@ -2,6 +2,7 @@
 
 namespace Isics\Bundle\OpenMiamMiamBundle\Tests\Twig;
 
+use Doctrine\ORM\EntityManager;
 use Isics\Bundle\OpenMiamMiamBundle\Entity\Category;
 use Isics\Bundle\OpenMiamMiamBundle\Twig\CategoryLevelExtension;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
@@ -24,23 +25,34 @@ class CategoryLevelExtensionTest extends WebTestCase
      * Method called before each test
      */
     public function setUp() {
+        // Truncate category table
+        // In order to select a category by her id
+        $this->truncateTable('Isics\Bundle\OpenMiamMiamBundle\Entity\Category');
+
         // Loading fixtures
-        $classes = array(
-            'Isics\Bundle\OpenMiamMiamBundle\DataFixtures\ORM\LoadUserData',
-            'Isics\Bundle\OpenMiamMiamBundle\DataFixtures\ORM\LoadAssociationData',
-            'Isics\Bundle\OpenMiamMiamBundle\DataFixtures\ORM\LoadBranchData',
-            'Isics\Bundle\OpenMiamMiamBundle\DataFixtures\ORM\LoadBranchOccurrenceData',
-            'Isics\Bundle\OpenMiamMiamBundle\DataFixtures\ORM\LoadCategoryData',
-            'Isics\Bundle\OpenMiamMiamBundle\DataFixtures\ORM\LoadProducerAttendanceData',
-            'Isics\Bundle\OpenMiamMiamBundle\DataFixtures\ORM\LoadProducerData',
-            'Isics\Bundle\OpenMiamMiamBundle\DataFixtures\ORM\LoadProductData'
-        );
-        $this->loadFixtures($classes);
+        self::runCommand('doctrine:fixtures:load', array('-n' => true, '--fixtures' => __DIR__ . '/../../DataFixtures/ORM/'));
 
         // Prepare the extension to test
         /** @var Translator $translator */
         $translator = $this->getContainer()->get('translator');
         $this->extension = new CategoryLevelExtension($translator);
+    }
+
+    /**
+     * Method to empty a table & reset auto increments
+     * 
+     * @param $classname
+     */
+    protected function truncateTable($classname) {
+        /** @var EntityManager $em */
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $cmd = $em->getClassMetadata($classname);
+        $connection = $em->getConnection();
+        $dbPlatform = $connection->getDatabasePlatform();
+        $connection->query('SET FOREIGN_KEY_CHECKS = 0');
+        $q = $dbPlatform->getTruncateTableSql($cmd->getTableName());
+        $connection->executeUpdate($q);
+        $connection->query('SET FOREIGN_KEY_CHECKS = 1');
     }
 
     /**
@@ -57,7 +69,7 @@ class CategoryLevelExtensionTest extends WebTestCase
      */
     public function testGetRootCategory() {
         // Getting the "Porc" category
-        $category = $this->getParticularCategoryByName('Porc');
+        $category = $this->getParticularCategory(7);
 
         // Ask for the level 0 category (root)
         $rootCategory = $this->extension->rootCategoryToLevel($category, 0);
@@ -69,7 +81,7 @@ class CategoryLevelExtensionTest extends WebTestCase
      */
     public function testGetLevelOneCategory() {
         // Getting the "Porc" category
-        $category = $this->getParticularCategoryByName('Porc');
+        $category = $this->getParticularCategory(7);
 
         // Ask for the parent category
         $motherCategory = $this->extension->rootCategoryToLevel($category);
@@ -81,7 +93,7 @@ class CategoryLevelExtensionTest extends WebTestCase
      */
     public function testGetSameCategory() {
         // Getting the "Porc" category
-        $category = $this->getParticularCategoryByName('Porc');
+        $category = $this->getParticularCategory(7);
 
         // Ask for the same category
         $sameCategory = $this->extension->rootCategoryToLevel($category, 2);
@@ -89,23 +101,19 @@ class CategoryLevelExtensionTest extends WebTestCase
     }
 
     /**
-     * Just return the object Category by the given name
+     * Just return the category by the given id
      *
-     * @param $name
+     * @param $id
      * @return Category
      */
-    protected function getParticularCategoryByName($name) {
+    protected function getParticularCategory($id) {
         return
             $this
                 ->getContainer()
                 ->get('doctrine')
                 ->getManager()
                 ->getRepository('IsicsOpenMiamMiamBundle:Category')
-                ->findOneBy(
-                    array(
-                        'name' => $name
-                    )
-                )
+                ->find($id)
             ;
     }
 }
