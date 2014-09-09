dfbce0c8f0d8586a6184633ab5a9ea99db842fce
Steve Robinet <steve.robinet@isics.fr>
2014-09-09 17:16:04 +0200

    Add an statistic attribute which say if it's a price attribute (so €)

--- src/Isics/Bundle/OpenMiamMiamBundle/Resources/assets/js/admin.js
+++ src/Isics/Bundle/OpenMiamMiamBundle/Resources/assets/js/admin.js
@@ -947,7 +947,9 @@ OpenMiamMiam.DashboardStatistics = function(){
 
             var currencyFormatter = new google.visualization.NumberFormat({pattern: '###,###.## €'});
             currencyFormatter.format(chartData, 1);
-            currencyFormatter.format(chartData, 2);
+            if(data.isPriceInformation) {
+                currencyFormatter.format(chartData, 2);
+            }
 
             var chart = new google.visualization.LineChart(this.$graphContainer[0]);
             chart.draw(chartData, options);
